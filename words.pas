1d28e47089218131738f74fef10caff19ecc419c
Смирнов Вадим <himixster@gmail.com>
2019-05-11 13:18:32 +0300

    front

--- words.pas
+++ words.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Label1: TLabel;
+    Edit1: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure Label1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +27,8 @@ var
 implementation
 
 {$R *.dfm}
+
+
+
 
 end.
