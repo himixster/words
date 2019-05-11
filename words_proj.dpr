96ca8caa3ee69aac2f4ef4fbd85c0685ce440c34
Смирнов Вадим <himixster@gmail.com>
2019-05-11 12:55:30 +0300

    start

---
+++ words_proj.dpr
@@ -0,0 +1,14 @@
+program words_proj;
+
+uses
+  Vcl.Forms,
+  words in 'words.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
